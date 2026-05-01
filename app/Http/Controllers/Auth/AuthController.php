fd661b9362ee32f65801f264dd302215baa8ad3a
adtzslowy <aditzkun0987@gmail.com>
2026-05-01 20:59:20 +0700

    feat: logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -36,4 +36,14 @@ class AuthController extends Controller
         ]);
     }
 
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login');
+    }
+
 }
